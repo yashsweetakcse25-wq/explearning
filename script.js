294abee7c0098d095a6d13e443ff4b51e40923f5
yashsweetakcse25-wq <yashsweetak.cse25@sbjit.edu.in>
2026-04-29 21:31:06 +0530

    Implement cart management and selection features
    Add cart functionality with size and color selection

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-function showPage(pageId) {
-    let pages = document.querySelectorAll('.page');
-    
-    pages.forEach(function(page) {
-        page.classList.remove('active-page');
-    });
+let cartCount = localStorage.getItem("cartCount") || 0;
+document.getElementById("cart-count").innerText = cartCount;
 
-    document.getElementById(pageId).classList.add('active-page');
+let selectedSize = "";
+let selectedColor = "";
+
+function selectSize(size) {
+    selectedSize = size;
+    document.getElementById("size-msg").innerText = "Selected Size: " + size;
+}
+
+function selectColor(color) {
+    selectedColor = color;
+    document.getElementById("color-msg").innerText = "Selected Color: " + color;
+}
+
+function addToCart() {
+    if (selectedSize === "" || selectedColor === "") {
+        alert("Select size & color first!");
+        return;
+    }
+
+    cartCount++;
+    localStorage.setItem("cartCount", cartCount);
+    document.getElementById("cart-count").innerText = cartCount;
+
+    alert("Added to cart!");
 }
