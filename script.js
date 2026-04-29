82f546fa5b60e9453af8ef113f6372a4d895aaa2
yashsweetakcse25-wq <yashsweetak.cse25@sbjit.edu.in>
2026-04-29 23:04:32 +0530

    Refactor option selection and cart addition logic
    Refactor selectOption and addToCart functions for clarity and remove duplicate code.

--- script.js
+++ script.js
@@ -1,42 +1,9 @@
-let selectedColor = "";
-let selectedSize = "";
+function showSection(id) {
+    let sections = document.querySelectorAll(".section");
 
-// Select color or size
-function selectOption(btn, type) {
+    sections.forEach(sec => {
+        sec.classList.remove("active");
+    });
 
-    // remove active from same group
-    let parent = btn.parentElement;
-    let buttons = parent.querySelectorAll(".option-btn");
-
-    buttons.forEach(b => b.classList.remove("active-option"));
-
-    btn.classList.add("active-option");
-
-    if (type === "color") {
-        selectedColor = btn.innerText;
-    } else {
-        selectedSize = btn.innerText;
-    }
-}
-
-// Add to cart validation
-function addToCart() {
-    if (selectedColor === "" || selectedSize === "") {
-        alert("Please select color and size first!");
-        return;
-    }
-    function selectOption(btn, type) {
-    let parent = btn.parentElement;
-    let buttons = parent.querySelectorAll(".option-btn");
-
-    buttons.forEach(b => b.classList.remove("active-option"));
-
-    btn.classList.add("active-option");
-}
-
-function addToCart() {
-    alert("Added to cart!");
-}
-
-    alert("Added to cart!\nColor: " + selectedColor + "\nSize: " + selectedSize);
+    document.getElementById(id).classList.add("active");
 }
